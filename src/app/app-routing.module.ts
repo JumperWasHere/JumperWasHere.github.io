0291c36df8ecf9cb1a76ac2947dc833954efb971
mohd ariff <mariffnajiuddin.abdh@petronas.com>
2023-10-05 09:36:03 +0800

    add select2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./form/form.module').then(m => m.FormModule) },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
